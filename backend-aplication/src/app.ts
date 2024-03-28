a1e0f6e10c21ebce6f9d7c33a9bfd62a33ef2ef4
Vinícius Branger <vinibranger@gmail.com>
2024-03-27 21:12:43 -0300

    importado dados mongodb

--- backend-aplication/src/app.ts
+++ backend-aplication/src/app.ts
@@ -1,19 +1,21 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 class App {
-public app: express.Application;
+  public app: express.Application;
 
-public constructor() {
-  this.app = express();
-  this.app.use(cors());
-}
+  public constructor() {
+    this.app = express();
+    this.app.use(cors());
+  }
 
-public listen(port: number): void {
-  this.app.listen(port, () => {
-    console.log(`Aplicação iniciada na porta: ${port}`);
-  });
-}
+  private initMongoose(): void {}
+
+  public listen(port: number): void {
+    this.app.listen(port, () => {
+      console.log(`Aplicação iniciada na porta: ${port}`);
+    });
+  }
 }
 
 export default App;
