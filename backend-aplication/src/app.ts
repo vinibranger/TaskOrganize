4099c2f5a976d33ce306ef58047da5f02ffc0a2b
Vinícius Branger <vinibranger@gmail.com>
2024-03-27 22:07:31 -0300

    Update app.ts

--- backend-aplication/src/app.ts
+++ backend-aplication/src/app.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import cors from "cors";
+import mongoose from "mongoose";
 
 class App {
   public app: express.Application;
@@ -7,9 +8,22 @@ class App {
   public constructor() {
     this.app = express();
     this.app.use(cors());
+    this.initMongoose();
+    this.connectDataBase();
   }
 
-  private initMongoose(): void {}
+  private initMongoose(): void {
+    mongoose.set("runValidators", true); //aqui é true para que as validação do schema funcione certo
+  }
+
+  private connectDataBase(): void {
+    mongoose.connect("mongodb+srv://vinibranger:2dqW6b06yvekte1I@cluster0.3te1hrw.mongodb.net/projetoCrud?retryWrites=true&w=majority&appName=Cluster0",{
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+        useCreateIndex: true,
+      });
+  }
 
   public listen(port: number): void {
     this.app.listen(port, () => {
