3dbf735459b7a0822c44b2a14f0f48cf3108f235
Vinícius Branger <vinibranger@gmail.com>
2024-03-28 20:47:01 -0300

    teste

--- backend-aplication/src/app.ts
+++ backend-aplication/src/app.ts
@@ -15,14 +15,9 @@ class App {
   private initMongoose(): void {
     mongoose.set("runValidators", true); //aqui é true para que as validação do schema funcione certo
   }
-
+  
   private connectDataBase(): void {
-    mongoose.connect("mongodb+srv://vinibranger:2dqW6b06yvekte1I@cluster0.3te1hrw.mongodb.net/projetoCrud?retryWrites=true&w=majority&appName=Cluster0",{
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-        useCreateIndex: true,
-      });
+    mongoose.connect("mongodb+srv://vinibranger:1fddMGUbq71sk9fb@cluster0.3te1hrw.mongodb.net/projetoCrud?retryWrites=true&w=majority&appName=Cluster0");
   }
 
   public listen(port: number): void {
@@ -33,3 +28,9 @@ class App {
 }
 
 export default App;
+
+
+///YGy5ZOKphG72ljFs
+
+
+//mongodb+srv://vinibranger:YGy5ZOKphG72ljFs@cluster0.3te1hrw.mongodb.net/
