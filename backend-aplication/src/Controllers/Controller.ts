7b0522d93e7dfbd17a8e476c13e13b341ddda7e5
Vinícius Branger <vinibranger@gmail.com>
2024-04-01 22:38:26 -0300

    controlers

---
+++ backend-aplication/src/Controllers/Controller.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+
+abstract class Controller {
+  protected path: string;
+  public router: Router;
+
+  constructor(path: string){
+    this.router= Router();
+    this.path = path;
+    setTimeout(() => {
+      this.initRoutes();
+    }, 0);
+  }
+ protected abstract initRoutes(): void;
+}
+
+export default Controller;
--- backend-aplication/src/app.ts
+++ backend-aplication/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import Controller from "./Controllers/Controller";
 
 class App {
   public app: express.Application;
@@ -8,17 +9,16 @@ class App {
   public constructor() {
     this.app = express();
     this.app.use(cors());
+
     this.initMongoose();
     this.connectDataBase();
+    this.initExpressJson();
   }
 
   private initMongoose(): void {
     mongoose.set("runValidators", true); //aqui é true para que as validação do schema funcione certo
   }
 
-  // private connectDataBase(): void {
-  //   mongoose.connect("mongodb+srv://vinibranger:1fddMGUbq71sk9fb@cluster0.3te1hrw.mongodb.net/projetoCrud?retryWrites=true&w=majority&appName=Cluster0");
-  //*  }
   private connectDataBase = async () => {
     try {
       await mongoose.connect(
@@ -29,6 +29,14 @@ class App {
       console.log("error while connecting mongodb", error.message);
     }
   };
+  private initExpressJson(): void {
+    this.app.use(express.json()); //Aqui eu defino que quero utilizar o padrao json para leitura e resposta
+  }
+  private initControllers(constrollers: Controller[]): void {
+    constrollers.forEach((Controller) => {
+      this.app.use("/", controller.router);
+    });
+  }
 
   public listen(port: number): void {
     this.app.listen(port, () => {
@@ -38,4 +46,3 @@ class App {
 }
 
 export default App;
-
