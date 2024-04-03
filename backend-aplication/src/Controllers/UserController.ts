5b29265866d0c19b4ec5be37fe32cd13aea66d03
Vinícius Branger <107061354+vinibranger@users.noreply.github.com>
2024-04-03 12:54:49 -0300

    Update UserController.ts

--- backend-aplication/src/Controllers/UserController.ts
+++ backend-aplication/src/Controllers/UserController.ts
@@ -10,6 +10,7 @@ class UserController extends Controller {
 
   protected initRoutes(): void {
     this.router.get(this.path, this.list);
+    this.router.get(this.path + "/:id", this.findById);
   }
 
   private async list(
@@ -27,8 +28,9 @@ class UserController extends Controller {
     next: NextFunction
   ): Promise<Response> {
     const { id } = req.params; //22:37
-
-    if(!Types.ObjectId.isValid(id))
+    if (!Types.ObjectId.isValid(id)) return res.status(400).send("ERROR");
+    const user = await User.findById(id);
+    return res.send(user);
   }
 }
 
