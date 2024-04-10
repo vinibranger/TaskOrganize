0ce110b453b73b982dc16869c66802c16981137f
Vinícius Branger <107061354+vinibranger@users.noreply.github.com>
2024-04-10 12:54:00 -0300

    Update ServerErrorException.ts

--- backend-aplication/src/Errors/ServerErrorException.ts
+++ backend-aplication/src/Errors/ServerErrorException.ts
@@ -3,30 +3,30 @@ import HttpException from "./HttpException";
 
 
 class ServerErrorException extends HttpException {
-  constructor(error) {
+  constructor(error: any) {
     super(getStatus(error), getMessage(error));
   }
 }
 
-function getStatus(error): number {
+function getStatus(error:any): number {
   if (isMongoException(error)) return HttpStatusCode.BAD_REQUEST;
   return HttpStatusCode.INTERNAL_SERVER_ERROR;
 }
 
-function isMongoException(error): boolean {
+function isMongoException(error:any): boolean {
   if (isMongoError(error) || isValidationError(error)) return true;
   return false;
 }
 
-function isMongoError(error): boolean {
+function isMongoError(error:any): boolean {
   return error.name === 'MongoError';
 }
 
-function isValidationError(error): boolean {
+function isValidationError(error:any): boolean {
   return error.name === 'ValidationError';
 }
 
-function getMessage(error): string {
+function getMessage(error:any): string {
   try {
     if (isMongoException(error)) {
       if (isKeyUniqueError(error)) return getMessageKeyUnique(error);
@@ -38,24 +38,25 @@ function getMessage(error): string {
   return getMessageGeneric();
 }
 
-function isKeyUniqueError(error) {
+function isKeyUniqueError(error:any) {
   return isMongoError(error) && error.code === 11000;
 }
-
-function getMessageKeyUnique(error): string {
+//Quando você usa any como tipo para uma variável ou parâmetro, você está essencialmente dizendo ao TypeScript para não realizar checagens de tipo nessa variável ou parâmetro.
+//Isso permite que você trabalhe com valores de qualquer tipo sem receber erros de tipo do TypeScript
+function getMessageKeyUnique(error:any): string {
   const { keyPattern } = error;
-  const listFormatedErros = [];
+  const listFormatedErros: string[] = [];
   Object.keys(keyPattern).forEach((field) => {
     listFormatedErros.push(`${field} deve ser único`);
   });
   return listFormatedErros.join(' | ');
 }
 
-function getMessageValidationError(error): string {
+function getMessageValidationError(error:any): string {
   const { errors } = error;
-  const listFormatedErros = [];
+  const listFormatedErros: string[] = [];
   Object.keys(errors).forEach((field: string) => {
-    listFormatedErros.push(errors[field].message);
+    listFormatedErros.push(errors[field].message);// ESSE SERIA PARA PEGAR A MSG DO REQUIRE DO USER NO CASO NOME/EMAIL 
   });
   return listFormatedErros.join(' | ');
 }
@@ -63,5 +64,8 @@ function getMessageValidationError(error): string {
 function getMessageGeneric(): string {
   return 'Erro interno no servidor.';
 }
+
+
+//AULA5 31:55
 
 export default ServerErrorException;
